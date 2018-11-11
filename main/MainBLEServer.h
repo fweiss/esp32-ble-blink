d1fb095dd2fdff5d8e056e6d56a11f5ed0aa6f22
Frank E Weiss <feweiss@gmail.com>
2018-11-11 11:10:51 -0800

    Refactor: extract battery level characteristic as create method and use lambda for callback

--- main/MainBLEServer.h
+++ main/MainBLEServer.h
@@ -33,9 +33,9 @@ class MainBLEServer: public Task {
             readCallback(characteristic);
         }
     private:
-        std::function<void(BLECharacteristic*)> readCallback;
-        std::function<void(BLECharacteristic*)> writeCallback;
-        const std::function<void(BLECharacteristic*)> nullCallback = [](BLECharacteristic*){};
+        CharacteristicCallback readCallback;
+        CharacteristicCallback writeCallback;
+        const CharacteristicCallback nullCallback = [](BLECharacteristic*){};
     };
 
 //    class HeartRateCallbacks : public BLECharacteristicCallbacks {
@@ -53,19 +53,19 @@ class MainBLEServer: public Task {
 //            blinker->setBeatsPerMinute(value);
 //        }
 //    };
-    class BatteryCallbacks : public BLECharacteristicCallbacks {
-    public:
-        BatteryLevel* battery;
-        BatteryCallbacks(BatteryLevel* battery) {
-            this->battery = battery;
-        }
-        virtual void onRead(BLECharacteristic* characteristic) override {
-//            int value = battery->getCurrentLevel();
-            uint8_t value = battery->toPercent(battery->getCurrentLevel());
-            characteristic->setValue(&value, 1);
-            ESP_LOGI(LOG_TAG, "battery level: %d", value);
-        }
-    };
+//    class BatteryCallbacks : public BLECharacteristicCallbacks {
+//    public:
+//        BatteryLevel* battery;
+//        BatteryCallbacks(BatteryLevel* battery) {
+//            this->battery = battery;
+//        }
+//        virtual void onRead(BLECharacteristic* characteristic) override {
+////            int value = battery->getCurrentLevel();
+//            uint8_t value = battery->toPercent(battery->getCurrentLevel());
+//            characteristic->setValue(&value, 1);
+//            ESP_LOGI(LOG_TAG, "battery level: %d", value);
+//        }
+//    };
     FlashingIndicator* blink;
     BatteryLevel* battery;
 
@@ -85,21 +85,22 @@ class MainBLEServer: public Task {
 
         BLEService* pService = pServer->createService("91bad492-b950-4226-aa2b-4ede9fa42f59");
 
-        BLECharacteristic* pCharacteristic = pService->createCharacteristic(
-            BLEUUID(characteristicUUID),
-            BLECharacteristic::PROPERTY_BROADCAST | BLECharacteristic::PROPERTY_READ  |
-            BLECharacteristic::PROPERTY_NOTIFY    | BLECharacteristic::PROPERTY_WRITE |
-            BLECharacteristic::PROPERTY_INDICATE
-        );
+//        BLECharacteristic* pCharacteristic = pService->createCharacteristic(
+//            BLEUUID(characteristicUUID),
+//            BLECharacteristic::PROPERTY_BROADCAST | BLECharacteristic::PROPERTY_READ  |
+//            BLECharacteristic::PROPERTY_NOTIFY    | BLECharacteristic::PROPERTY_WRITE |
+//            BLECharacteristic::PROPERTY_INDICATE
+//        );
+//
+//        uint8_t batteryLevel = battery->getCurrentLevel(); //57;
+//        pCharacteristic->setValue(&batteryLevel, sizeof(batteryLevel));
+//        pCharacteristic->setCallbacks(new BatteryCallbacks(battery));
+//
+//        BLE2902* p2902Descriptor = new BLE2902();
+//        p2902Descriptor->setNotifications(true);
+//        pCharacteristic->addDescriptor(p2902Descriptor);
 
-        uint8_t batteryLevel = battery->getCurrentLevel(); //57;
-        pCharacteristic->setValue(&batteryLevel, sizeof(batteryLevel));
-        pCharacteristic->setCallbacks(new BatteryCallbacks(battery));
-
-        BLE2902* p2902Descriptor = new BLE2902();
-        p2902Descriptor->setNotifications(true);
-        pCharacteristic->addDescriptor(p2902Descriptor);
-
+        createBatteryLevelCharacteristic(pService);
         createHeartRateCharacteristic(pService);
 
         pService->start();
@@ -111,6 +112,29 @@ class MainBLEServer: public Task {
         ESP_LOGD(LOG_TAG, "Advertising started!");
         // todo find a better way to suspend here
         delay(1000000);
+    }
+
+    void createBatteryLevelCharacteristic(BLEService* pService) {
+        BLECharacteristic* pCharacteristic = pService->createCharacteristic(
+            BLEUUID(characteristicUUID),
+            BLECharacteristic::PROPERTY_BROADCAST | BLECharacteristic::PROPERTY_READ  |
+            BLECharacteristic::PROPERTY_NOTIFY    | BLECharacteristic::PROPERTY_WRITE |
+            BLECharacteristic::PROPERTY_INDICATE
+        );
+
+        uint8_t batteryLevel = battery->getCurrentLevel(); //57;
+        pCharacteristic->setValue(&batteryLevel, sizeof(batteryLevel));
+//        pCharacteristic->setCallbacks(new BatteryCallbacks(battery));
+        pCharacteristic->setCallbacks(createReadCallbacks(
+            [this](BLECharacteristic* characteristic) {
+                uint8_t value = battery->toPercent(battery->getCurrentLevel());
+                characteristic->setValue(&value, 1);
+                ESP_LOGI(LOG_TAG, "battery level: %d", value);
+        }));
+
+        BLE2902* p2902Descriptor = new BLE2902();
+        p2902Descriptor->setNotifications(true);
+        pCharacteristic->addDescriptor(p2902Descriptor);
     }
     void createHeartRateCharacteristic(BLEService* service) {
         const char* characteristicUUID = "00002A92-0000-1000-8000-00805F9B34FB";
